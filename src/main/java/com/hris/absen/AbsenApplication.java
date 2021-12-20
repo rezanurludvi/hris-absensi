9e5d35e874358cc2e0412e8a78a183ab310cd0a6
rezanurludvi <rezanurludvi@gmail.com>
2021-12-20 15:18:34 +0700

    first commit

---
+++ src/main/java/com/hris/absen/AbsenApplication.java
@@ -0,0 +1,13 @@
+package com.hris.absen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenApplication.class, args);
+	}
+
+}
